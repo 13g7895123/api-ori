b9d6858aa68ad6d5a2b6d5fc14ae6146e2aaf979
Jarvis <13g7895123@gmail.com>
2023-08-18 22:52:53 +0800

    [edit] test api

--- api-ori/api/bonus-register/tools.php
+++ api-ori/api/bonus-register/tools.php
@@ -5,8 +5,8 @@ class tool
     // 歐買尬
     public static function omgms()
     {
-        // $api_token = base64_encode('90339cff-6d61-4b85-a123-b03a090635ef');
-        $api_token = '90339cff-6d61-4b85-a123-b03a090635ef';
+        $api_token = base64_encode('90339cff-6d61-4b85-a123-b03a090635ef');
+        // $api_token = '90339cff-6d61-4b85-a123-b03a090635ef';
         $url = 'https://api.omgms.com.tw ';
         $data = array(
             'Destination' => '0903706726',
@@ -18,7 +18,7 @@ class tool
         $curl = curl_init();
         $header = array();
         $header[] = 'Content-type: application/x-www-form-urlencoded';
-        $header[] = 'Authorization: Base64 '.$api_token;
+        $header[] = 'Authorization: '.$api_token;
 
         curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
         curl_setopt($curl, CURLOPT_URL, $url);
