40cf90cd5e845057e6554f05b620cf2f58950be8
ict005 <13g7895123@gmail.com>
2023-08-25 13:24:05 +0800

    [edit] change db config

--- api-ori/api/bonus-register/config.php
+++ api-ori/api/bonus-register/config.php
@@ -3,8 +3,10 @@ include_once(__DIR__ . '/../../__Class/ClassLoad.php');
 
 MYPDO::$host = '139.162.15.125';
 MYPDO::$port = '3306';
-MYPDO::$db = 'db_test';
-MYPDO::$user = 'test_remote';
+// MYPDO::$db = 'db_test';
+MYPDO::$db = 'db_bonus_register';
+// MYPDO::$user = 'test_remote';
+MYPDO::$user = 'bonus_register_remote';
 MYPDO::$pwd = '820820';
 
 ?>
