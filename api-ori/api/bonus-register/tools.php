65601c6c6d292e9fd5069b46eb2ea5a724e16eaf
Jarvis <13g7895123@gmail.com>
2023-08-27 15:26:20 +0800

    [edit] test function process

--- api-ori/api/bonus-register/tools.php
+++ api-ori/api/bonus-register/tools.php
@@ -23,13 +23,14 @@ class tools
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_POST, 1);
         curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
         // 執⾏
         $output = curl_exec($curl);
         curl_close($curl);
-        // return $output;
+        return $output;
 
-        echo $output;
+        // echo $output;
     }
 
     // 簡訊-三竹(需要公司行號才可使用)
