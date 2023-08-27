893b6e519629d84fb8ea13fff3857f6a5d813f13
Jarvis <13g7895123@gmail.com>
2023-08-27 15:31:00 +0800

    [edit] test function process

--- api-ori/api/bonus-register/tools.php
+++ api-ori/api/bonus-register/tools.php
@@ -23,7 +23,7 @@ class tools
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_POST, 1);
         curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);  // 取得回傳資料
 
         // 執⾏
         $output = curl_exec($curl);
